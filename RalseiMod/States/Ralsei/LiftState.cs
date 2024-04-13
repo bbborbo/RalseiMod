88f7b82f7061948a96cd3edd5ce1d2307a2c2b94
BORBO <bwofodol@gmail.com>
2024-04-13 15:45:00 -0500

    fixed lift

--- RalseiMod/States/Ralsei/LiftState.cs
+++ RalseiMod/States/Ralsei/LiftState.cs
@@ -1,6 +1,7 @@
 ﻿using EntityStates;
 using EntityStates.VoidSurvivor;
 using RalseiMod.Skills;
+using RalseiMod.Survivors.Ralsei;
 using RoR2;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,6 @@ namespace RalseiMod.States.Ralsei
     class LiftState : GenericCharacterMain
     {
         static bool useMoveSpeed => LiftPrayer.useMoveSpeed;
-        float liftRateStart;
         float liftRateMin;
         float liftRateMax;
         float duration;
@@ -20,7 +20,6 @@ namespace RalseiMod.States.Ralsei
         {
             liftRateMin = LiftPrayer.liftSpeedMin * this.attackSpeedStat;
             liftRateMax = LiftPrayer.liftSpeedMax * this.attackSpeedStat;
-            liftRateStart = liftRateMax > 0 ? liftRateMin / liftRateMax : 0;
             duration = LiftPrayer.liftDuration / this.attackSpeedStat;
             base.OnEnter();
         }
@@ -53,12 +52,16 @@ namespace RalseiMod.States.Ralsei
 
         private float GetLiftVelocity()
         {
-            AnimationCurve upSpeed = AnimationCurve.EaseInOut(0, liftRateStart, 1, 1);
+            AnimationCurve upSpeed = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
             float time = base.fixedAge / this.duration;
-            float b = Util.Remap(upSpeed.Evaluate(time), liftRateStart, 1, liftRateMin, liftRateMax);
+            float b = Util.Remap(upSpeed.Evaluate(time), 0, 1, liftRateMin, liftRateMax);
+
             if (useMoveSpeed)
                 b *= this.moveSpeedStat;
+            else
+                b *= RalseiSurvivor.ralseiMoveSpeed;
+
             return b;
         }
 
